dc027d7da51b0a4ec7d2ca577c26645f2f385d8d
Andrei Stoica <andrei.stoica.365@gmail.com>
2024-02-02 13:24:32 -0500

    zen mode

--- .nvchad-custom/plugins.lua
+++ .nvchad-custom/plugins.lua
@@ -1,117 +1,132 @@
-local overrides = require "custom.configs.overrides"
-
+local overrides = require("custom.configs.overrides")
 
 ---@type NvPluginSpec[]
 local plugins = {
+	{
+		"folke/twilight.nvim",
+		opts = {
+			context = 20,
+			expand = {
+				"function",
+			},
+		},
+		lazy = false,
+	},
+	{
+		"folke/zen-mode.nvim",
+		opts = {
+			tmux = {
+				enable = false,
+			},
+		},
+		lazy = false,
+	},
+	{
+		"christoomey/vim-tmux-navigator",
+		cmd = {
+			"TmuxNavigateLeft",
+			"TmuxNavigateDown",
+			"TmuxNavigateUp",
+			"TmuxNavigateRight",
+			"TmuxNavigatePrevious",
+		},
+		lazy = false,
+	},
+	-- Override plugin definition options
 
-  {
-    "christoomey/vim-tmux-navigator",
-    cmd = {
-      "TmuxNavigateLeft",
-      "TmuxNavigateDown",
-      "TmuxNavigateUp",
-      "TmuxNavigateRight",
-      "TmuxNavigatePrevious",
-    },
-    lazy = false,
-  },
-  -- Override plugin definition options
+	{
+		"hrsh7th/nvim-cmp",
+		opts = overrides.cmp,
+	},
 
-  {
-    "hrsh7th/nvim-cmp",
-    opts = overrides.cmp
-  },
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			-- format & linting
+			{
+				"jose-elias-alvarez/null-ls.nvim",
+				config = function()
+					require("custom.configs.null-ls")
+				end,
+			},
+		},
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end, -- Override to setup mason-lspconfig
+	},
 
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      -- format & linting
-      {
-        "jose-elias-alvarez/null-ls.nvim",
-        config = function()
-          require "custom.configs.null-ls"
-        end,
-      },
-    },
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end, -- Override to setup mason-lspconfig
-  },
+	-- override plugin configs
+	{
+		"williamboman/mason.nvim",
+		opts = overrides.mason,
+	},
 
-  -- override plugin configs
-  {
-    "williamboman/mason.nvim",
-    opts = overrides.mason,
-  },
+	{
+		"williamboman/mason-lspconfig.nvim",
+		setup = {
+			ensure_installed = { "jedi" },
+		},
+	},
 
-  {
-    "williamboman/mason-lspconfig.nvim",
-    setup = {
-      ensure_installed = { "jedi" },
-    },
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = overrides.treesitter,
+	},
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = overrides.treesitter,
-  },
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = overrides.nvimtree,
+	},
 
-  {
-    "nvim-tree/nvim-tree.lua",
-    opts = overrides.nvimtree,
-  },
+	-- Install a plugin
+	{
+		"max397574/better-escape.nvim",
+		event = "InsertEnter",
+		config = function()
+			require("better_escape").setup()
+		end,
+	},
 
-  -- Install a plugin
-  {
-    "max397574/better-escape.nvim",
-    event = "InsertEnter",
-    config = function()
-      require("better_escape").setup()
-    end,
-  },
+	{ "nvim-treesitter/nvim-treesitter-context", lazy = false },
 
-  { "nvim-treesitter/nvim-treesitter-context",
-    lazy = false,
-  },
+	{
+		"theprimeagen/harpoon",
+	},
 
-  {
-    "theprimeagen/harpoon",
-  },
+	{
+		"simrat39/rust-tools.nvim",
+	},
 
-  {
-	  'simrat39/rust-tools.nvim'
-  },
-
-  {
-	  "ellisonleao/glow.nvim",
-	  config = true,
-	  cmd = "Glow",
-	  event = 'BufEnter *.md',
-  },
+	{
+		"ellisonleao/glow.nvim",
+		config = true,
+		cmd = "Glow",
+		event = "BufEnter *.md",
+	},
 
 	-- Disable nvchad plugins
 
-  {
-    "tpope/vim-sleuth",
-    lazy = false,
-  },
+	{
+		"tpope/vim-sleuth",
+		lazy = false,
+	},
 
-  { "NvChad/nvterm", enabled = false },
+	{ "NvChad/nvterm",                           enabled = false },
 
-  -- To make a plugin not be loaded
-  -- {
-  --   "NvChad/nvim-colorizer.lua",
-  --   enabled = false
-  -- },
+	-- To make a plugin not be loaded
+	-- {
+	--   "NvChad/nvim-colorizer.lua",
+	--   enabled = false
+	-- },
 
-  -- All NvChad plugins are lazy-loaded by default
-  -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
-  -- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
-  -- {
-  --   "mg979/vim-visual-multi",
-  --   lazy = false,
-  -- }
+	-- All NvChad plugins are lazy-loaded by default
+	-- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
+	-- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
+	-- {
+	--   "mg979/vim-visual-multi",
+	--   lazy = false,
+	-- }
 }
 
 return plugins
