de54ccfc012c3f1f76fb3c0d881d65ee22619028
Andrei Stoica <andrei.stoica.365@gmail.com>
2024-02-05 15:15:20 -0500

    updated navigation

--- .nvchad-custom/plugins.lua
+++ .nvchad-custom/plugins.lua
@@ -21,21 +21,7 @@ local plugins = {
 		lazy = false,
 	},
 	{
-		"christoomey/vim-tmux-navigator",
-		cmd = {
-			"TmuxNavigateLeft",
-			"TmuxNavigateDown",
-			"TmuxNavigateUp",
-			"TmuxNavigateRight",
-			"TmuxNavigatePrevious",
-		},
-		kesy = {
-			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-		},
+		"alexghergh/nvim-tmux-navigation",
 		lazy = false,
 	},
 	-- Override plugin definition options
